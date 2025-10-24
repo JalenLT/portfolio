6a4cdf5ecd51f8469aec407fd177dd70989aef0e
Stefan Seunarine <seunarine.stefan.lt@gmail.com>
2025-10-24 11:59:34 -0400

    feat(cards): add link to github repo on project cards

--- components/ui/project_card.tsx
+++ components/ui/project_card.tsx
@@ -7,23 +7,25 @@ const jost = Jost({
     subsets: ["latin"],
 });
 
-export default function ProjectCard({name, description, tags, icon}: {name: string, description: string, tags: string[], icon: React.ReactNode}) {
+export default function ProjectCard({name, description, tags, icon, href}: {name: string, description: string, tags: string[], icon: React.ReactNode, href: string}) {
     return (
-        <CustomCard>
-            <div className="flex">
-                {icon}
-                <h1 className={`text-[#746352] text-3xl md:text-4xl font-semibold tracking-tight text-shadow-md mb-3 flex-grow ${jost.className}`}>
-                    {name}
-                </h1>
-            </div>
-            <p className="text-[#a89989] text-lg font-semibold text-shadow-sm mb-4">
-                {description}
-            </p>
-            <div className="flex">
-                {tags.map((tag, index) => (
-                    <Badge key={"tag-" + name + "-" + index} className="border-[#a89989] text-[#a89989] mx-1">{tag}</Badge>
-                ))}
-            </div>
-        </CustomCard>
+        <a target="_blank" href={href}>
+            <CustomCard>
+                <div className="flex">
+                    {icon}
+                    <h1 className={`text-[#746352] text-3xl md:text-4xl font-semibold tracking-tight text-shadow-md mb-3 flex-grow ${jost.className}`}>
+                        {name}
+                    </h1>
+                </div>
+                <p className="text-[#a89989] text-lg font-semibold text-shadow-sm mb-4">
+                    {description}
+                </p>
+                <div className="flex">
+                    {tags.map((tag, index) => (
+                        <Badge key={"tag-" + name + "-" + index} className="border-[#a89989] text-[#a89989] mx-1">{tag}</Badge>
+                    ))}
+                </div>
+            </CustomCard>
+        </a>
     );
 }
--- components/ui/project_cards.tsx
+++ components/ui/project_cards.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "./project_card";
 export default function ProjectCards(){
     return (<>
         {ProjectCardData.map((card, index) => {
-            return <ProjectCard key={"project-card-" + index} name={card.name} description={card.description} tags={card.tags} icon={card.icon} />;
+            return <ProjectCard key={"project-card-" + index} name={card.name} description={card.description} tags={card.tags} icon={card.icon} href={card.href} />;
         })}
     </>);
 }
