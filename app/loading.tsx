d9cb2e9e56985a16f5903414fd64ac87497b2b41
Stefan Seunarine <127210112+JalenLT@users.noreply.github.com>
2025-11-23 23:00:33 -0400

    feat(ui): update loading icon

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading(){
 	return (
-		<div className="relative h-[85vh] w-[85vw]">
-			<div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[200px] h-[200px] bg-[#746352] animate-pulse rounded-full"></div>
+		<div className="relative h-[85vh] w-100">
+			<div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[200px] h-[200px] bg-[#746352] text-2xl animate-pulse rounded-full flex items-center justify-center"><span className="text-white">Loading</span></div>
 		</div>
 	);
 }
